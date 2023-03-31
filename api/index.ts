347d33c4d6c2ae182c20cd8a16a8ab313b668c22
deybyr647 <deybyr647@gmail.com>
2023-03-30 20:39:52 -0400

    Added Prettier & MongoDB code

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,42 @@
-const app = require('express')();
-const { v4 } = require('uuid');
+import express, { Express, Request, Response } from "express";
 
-app.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+const app: Express = express();
+const { v4 } = require("uuid");
+
+/* GET ENDPOINTS */
+app.get("/api", (req: Request, res: Response) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
-    const { slug } = req.params;
-    res.end(`Item: ${slug}`);
+app.get("/api/item/:slug", (req: Request, res: Response) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+/* POST ENDPOINTS */
+app.post("/api", (req: Request, res: Response) => {
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+    key: process.env.API_KEY,
+    method: req.method,
+  });
+});
+
+app.post("/api/people/:name", (req: Request, res: Response) => {
+  const { name } = req.params;
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+    key: process.env.API_KEY,
+    message: `Hello from the API, ${name}`,
+    method: req.method,
+  });
 });
 
 module.exports = app;
--- api/next.ts
+++ api/next.ts
@@ -1,11 +1,33 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+import client from "../util/mongo-config";
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
-    res.status(200).json({
-        body: req.body,
-        query: req.query,
-        cookies: req.cookies,
-        key: process.env.API_KEY,
-        message: 'Hello from the API!',
-    });
-}
+/* Vercel/Next.js API Route */
+const handler = async (req: VercelRequest, res: VercelResponse) => {
+  // Connect to the MongoDB database
+  await client.connect();
+
+  // Get the database
+  const db = client.db("deybyr647-portfolio");
+
+  // Get the collection
+  const projects = db.collection("projects");
+
+  // Get all the documents
+  const documents = await projects.find().toArray();
+
+  // Close the connection
+  await client.close();
+
+
+  res.status(200).json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+    key: process.env.API_KEY,
+    message: "Hello from the API!",
+    method: req.method,
+    projects: documents,
+  });
+};
+
+export default handler;
